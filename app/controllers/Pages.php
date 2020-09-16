aa33c43941768e8750d449c68716c3542d9707d2
Pernilla Sterner <psterner@hotmail.se>
2020-09-16 11:30:29 +0200

    Add the MVC-Framwork setup

---
+++ app/controllers/Pages.php
@@ -0,0 +1,26 @@
+<?php
+
+class Pages extends Controller
+{
+  public function __construct()
+  {
+    
+  }
+
+  public function index()
+  {
+    $data = [
+      'title' => 'Basic MVC-Framework',
+    ];
+
+    $this->view('pages/index', $data);
+  }
+
+  public function about()
+  {
+    $data = [
+      'title' => 'about us'
+    ];
+    $this->view('pages/about', $data); 
+  }
+}
---
+++ app/views/pages/index.php
@@ -0,0 +1,3 @@
+<?php require APPROOT . '/views/inc/header.php'; ?>
+  <hi><?php echo $data['title']; ?></hi>
+<?php require APPROOT . '/views/inc/footer.php'; ?>
