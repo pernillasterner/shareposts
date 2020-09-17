3ed022a65c01a5dc8d1550ebd6421edd42fc454b
Pernilla Sterner <psterner@hotmail.se>
2020-09-17 15:32:03 +0200

    Create Session to store logged in userand logout func

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -140,7 +140,7 @@ class Users extends Controller
         $loggedInUser = $this->userModel->login($data['email'], $data['password']);
         if($loggedInUser) {
           // Create Session
-          die('SUCCESS');
+          $this->createUserSession($loggedInUser);
         } else {
           $data['password_err'] = 'Password incorrect';
           $this->view('users/login', $data);
@@ -161,6 +161,32 @@ class Users extends Controller
 
       // Load view
       $this->view('users/login', $data);
+    }
+  }
+
+  public function createUserSession($user)
+  {
+    $_SESSION['user_id'] = $user->id;
+    $_SESSION['user_email'] = $user->email;
+    $_SESSION['user_name'] = $user->name;
+    redirect('pages/index');
+  }
+
+  public function logout()
+  {
+    unset($_SESSION['user_id']);
+    unset($_SESSION['user_email']);
+    unset($_SESSION['user_name']);
+    session_destroy();
+    redirect('users/login');
+  }
+
+  public function isLoggedIn()
+  {
+    if(isset($_SESSION['user_id'])) {
+      return true;
+    } else {
+      return false;
     }
   }
 }
