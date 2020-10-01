ca0f7f6be4ec46110e55fcc7f6d143b18d575f18
Pernilla Sterner <psterner@hotmail.se>
2020-10-01 13:54:47 +0200

    Insert form data to DB and display data in Post View

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -21,11 +21,52 @@ class Posts extends Controller
   }
 
   public function add()
-  {
-    $data = [
-      'title' => '',
-      'body' => ''
-    ];
-    $this->view('posts/add', $data);
+  { 
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+      // Sanitize POST data
+      $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+      $data = [
+        'title' => trim($_POST['title']),
+        'body' => trim($_POST['body']),
+        'user_id' => $_SESSION['user_id'],
+        'title_err' => '',
+        'body_err' => ''
+      ];
+
+      // Validate data
+      if(empty($data['title'])) {
+        $data['title_err'] = 'Please enter title';
+      }
+
+      if(empty($data['body'])) {
+        $data['body_err'] = 'Please enter content';
+      }
+
+      // Make sure no errors
+      if(empty($data['title_err']) && empty($data['body_err'])) {
+        
+        // Validated
+        if($this->postModel->addPost($data)) {
+          flash('post_message', 'Post Added');
+          redirect('posts');
+        } else {
+          die('Something went wrong');
+        }
+
+      } else {
+        // Load view with errors
+        $this->view('posts/add', $data);
+      }
+
+    } else {
+      // Displaying the form
+      $data = [
+        'title' => '',
+        'body' => ''
+      ];
+      $this->view('posts/add', $data);
+    }
   }
 }
