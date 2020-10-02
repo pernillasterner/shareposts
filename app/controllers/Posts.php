2bbaed507c7952b3cdc5f6d9b325a96a866e9dbb
Pernilla Sterner <psterner@hotmail.se>
2020-10-02 11:22:06 +0200

    Delete posts & display user_name in nav

--- app/controllers/Posts.php
+++ app/controllers/Posts.php
@@ -73,6 +73,7 @@ class Posts extends Controller
 
   public function edit($id)
   { 
+    
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
       // Sanitize POST data
@@ -144,5 +145,28 @@ class Posts extends Controller
      'user' => $user
     ];
     $this->view('posts/show', $data);
+  }
+
+  public function delete($id)
+  {
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      // Get existing post from model
+      $post = $this->postModel->getPostById($id);
+
+      // Check for owner
+      if($post->user_id != $_SESSION['user_id']) {
+        redirect('posts');
+      } 
+
+      if($this->postModel->deletePost($id)) {
+        flash('post_message', 'Post Removed');
+        redirect('posts');
+      } else {
+        die('Something went wrong!');
+      }
+    } else {
+      redirect('posts');
+    }
   }
 }
